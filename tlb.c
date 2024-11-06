d37d149491d199341079f4806677699db7c9f3b1
RemiWhite30 <143155255+RemiWhite30@users.noreply.github.com>
2024-11-06 02:39:46 +0000

    Finished code and questions.

--- tlb.c
+++ tlb.c
@@ -1,10 +1,10 @@
 /*
 Remi White
 1. The timer is precise to the microsecond. It would need to be at least a few microseconds long before it to be timed precisely because it rounds to the nearest mircrosecond. 
-2. 
-5.
-6.
-7.
+2. Done
+5. You can ensure it does not remove the main loop if you disable optimization when compiling the file with the gcc flag -O0. 
+6. You can do it by binding a process to a CPU. If the code is bouncing from CPU to CPU, you do not get the benefits of cache affinity with the TLB.
+7. This will affect the timing a little bit. You could counterbalance it by initializing the array to begin with or by keeping track of the time it takes to initialize it and subtract that from the total time.
 */
 
 
@@ -15,12 +15,13 @@ Remi White
 #include <unistd.h>
 
 int main(int argc, char* argv[]) {
-    struct timeval start;
+    //initialize the gettimeofday info.
+    struct timeval start; 
     struct timeval end;
 
-    int PAGESIZE = 10;
-    int NUMPAGES = atoi(argv[1]);
-    int TRIALS = atoi(argv[2]);
+    int PAGESIZE = 10; //I just chose a random number.
+    int NUMPAGES = atoi(argv[1]); //First inputted argument gives number of pages.
+    int TRIALS = atoi(argv[2]); //Second inputted argument gives number of trials. 
     int jump = PAGESIZE / sizeof(int);
     int *a = calloc(NUMPAGES, PAGESIZE);
 
@@ -32,6 +33,7 @@ int main(int argc, char* argv[]) {
     }   
     gettimeofday(&end,NULL);
     
+    //Takes the time before the loops and subtracts it from the time after to figure out how long it took.
     printf("%ld seconds %ld microseconds\n", end.tv_sec-start.tv_sec, end.tv_usec-start.tv_usec);
    
     return 0;
